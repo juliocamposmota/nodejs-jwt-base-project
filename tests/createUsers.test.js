37e1e10ff0441f4bbdd2a9fa0d214c1bfd4559a7
Júlio Mota <juliomotacampos@gmail.com>
2021-11-30 17:54:45 -0300

    Implementa mock para isolar o model

--- tests/createUsers.test.js
+++ tests/createUsers.test.js
@@ -1,5 +1,10 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
 const server = require('../src/api/app');
 
 chai.use(chaiHttp);
@@ -10,13 +15,30 @@ describe('POST /api/users', () => {
   describe('quando é criado com sucesso', () => {
     let response = {};
 
+    const DBServer = new MongoMemoryServer();
+
     before(async () => {
+      const URLMock = await DBServer.getUri();
+      
+      const connectionMock = await MongoClient.connect(
+        URLMock,
+        { useNewUrlParser: true, useUnifiedTopoly: true },
+      );
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
       response = await chai.request(server)
         .post('/api/users')
         .send({
           username: 'Jane',
           password: 'senha123',
         });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
     });
 
     it('retorna o código de status 201', () => {
